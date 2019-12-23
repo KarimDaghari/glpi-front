9809b64408d7f63c2d5c7919897fa14a4616877a
Karim Daghari <karimdxy@gmail.com>
2019-12-23 17:17:38 +0100

    feat(): add auth guard

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,14 +2,24 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
 import Login from "../views/Login.vue";
+import store from "@/store";
 
 Vue.use(VueRouter);
+
+const authGuard = (to: any, from: any, next: Function) => {
+  if (!store.state.auth.isLoggedIn) {
+    next({ path: "/login", replace: true });
+  } else {
+    next();
+  }
+};
 
 const routes = [
   {
     path: "/",
     name: "home",
-    component: Home
+    component: Home,
+    beforeEnter: authGuard
   },
   {
     path: "/login",
@@ -23,7 +33,8 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+    beforeEnter: authGuard
   }
 ];
 
