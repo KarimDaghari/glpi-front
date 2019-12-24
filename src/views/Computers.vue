52c0cb5942f31f00405047691578a54979f34616
Karim Daghari <karimdxy@gmail.com>
2019-12-24 01:15:26 +0100

    feat(): add delete action

--- src/views/Computers.vue
+++ src/views/Computers.vue
@@ -1,10 +1,33 @@
 <template>
   <div>
+    <el-dialog
+      title="Actions"
+      :visible.sync="dialogVisible"
+      width="30%"
+      :before-close="handleClose"
+    >
+      <div>
+        <span>
+          Que souhaitez-vous faire ?
+        </span>
+        <div style="margin-top: 1em">
+          <el-select v-model="selectedOption">
+            <el-option label="Supprimer" value="deleteItem"></el-option>
+            <el-option label="Editer" value="editItem"></el-option>
+          </el-select>
+        </div>
+      </div>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="dialogVisible = false">Annuler</el-button>
+        <el-button type="primary" @click="executeAction">Confirmer</el-button>
+      </span>
+    </el-dialog>
+
     <div style="float: left">
-      <el-button>Actions</el-button>
+      <el-button @click="dialogVisible = true">Actions</el-button>
       <el-button icon="el-icon-plus" circle></el-button>
     </div>
-    <el-table :data="tableData" stripe>
+    <el-table :data="tableData" stripe @selection-change="addToList">
       <el-table-column type="selection" width="55"> </el-table-column>
       <el-table-column label="Nom" sortable prop="name"></el-table-column>
       <el-table-column label="Entité" sortable prop="entity"></el-table-column>
@@ -54,6 +77,32 @@ export default class extends Vue {
     lastModified: "2019-12-23",
     cpu: "i7-1056U"
   };
+  dialogVisible = false;
+  selectedOption: string = "";
+  selectedItems: any[] = [];
+
+  handleClose(done: any) {
+    this.$confirm("Etes-vous sûr(e) ?")
+      .then(_ => {
+        done();
+      })
+      .catch(_ => {});
+  }
+
+  deleteItem(items: any[]) {
+    this.tableData = this.tableData.filter(col => !items.includes(col));
+  }
+
+  addToList(val: any[]) {
+    this.selectedItems = val;
+  }
+
+  executeAction() {
+    const [action, selectedItems] = [this.selectedOption, this.selectedItems];
+    // @ts-ignore
+    this[action](selectedItems);
+    this.dialogVisible = false;
+  }
 
   mounted() {
     for (let i = 0; i < 10; i++) {
