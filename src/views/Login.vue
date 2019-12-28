3275f7a15a5fdbe9b4cdc75cb0f9236e065b5a82
Karim Daghari <karimdxy@gmail.com>
2019-12-28 12:25:43 +0100

    init repo

---
+++ src/views/Login.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <el-row></el-row>
+    <el-row>
+      <el-card>
+        <div>
+          <el-input prefix-icon="el-icon-user"></el-input>
+          <el-input prefix-icon="el-icon-lock" show-password></el-input>
+        </div>
+      </el-card>
+    </el-row>
+    <el-row>
+      <el-button style="primary" round>Continuer</el-button>
+    </el-row>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
